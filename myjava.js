3c29c08e19db6d87eab458208fcbcf358116db5b
LuqmanM19 <45591399+LuqmanM19@users.noreply.github.com>
2020-07-31 23:46:06 +0800

    Update myjava.js

--- myjava.js
+++ myjava.js
@@ -10,23 +10,35 @@ $(document).ready(function(){
     var $a = $('.a'),
     $b = $('.b'),
     $c = $('.c'),
+    $d = $('.d'),
     $home = $('.home'),
     $aboutme = $('.aboutme');
     $gallery = $('.gallery'),
+    $poster = $('.poster'),
 
     $a.click(function(){
         $home.fadeIn();
         $aboutme.fadeOut();
         $gallery.fadeOut();
+        $poster.fadeOut();
     });
     $b.click(function(){
         $aboutme.fadeIn();
         $home.fadeOut();
         $gallery.fadeOut();
+        $poster.fadeOut();
     });
     $c.click(function(){
         $gallery.fadeIn();
         $home.fadeOut();
         $aboutme.fadeOut();
+        $poster.fadeOut();
     });
+    $d.click(function(){
+        $poster.fadeIn();
+        $home.fadeOut();
+        $aboutme.fadeOut();
+        $gallery.fadeOut();
+    });
+    
 });
